f06618950a3105878983a7aa62a02da5d1f286c8
deconstruction10 <andrii.barchuk@yahoo.com>
2025-02-24 05:44:20 +0200

    Added project structure

---
+++ src/app/core/features/diary-list/diary-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diary-list',
+  imports: [],
+  templateUrl: './diary-list.component.html',
+  styleUrl: './diary-list.component.scss'
+})
+export class DiaryListComponent {
+
+}
